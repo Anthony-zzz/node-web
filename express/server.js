7b91767c054d5032fbbc8e553a21ba533e8f61f0
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-04-30 14:42:55 -0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,11 +1,9 @@
-const express = require ('express');
-const server = express ();
-server.get ('/', (request, response) => {
-  response.send (
-    'Servidor Express funcionando!\\nVocê está na página inicial.'
-  );
-});
-const PORT = 3000;
-server.listen (PORT, () => {
-  console.log (`Servidor Express iniciado em http://localhost:${PORT}/`);
-});
+const express = require('express')
+const server = express()
+server.get('/', (request, response) => { response.send('Servidor Express funcionando!\\nVocê está na página inicial.')
+})
+server.get('/artigos', (req, res) => { res.send('Você está na página "artigos".')
+})
+const PORT = 3000
+server.listen(PORT, () => { console.log(`Servidor Express iniciado em <http://localhost>:${PORT}/`)
+})
